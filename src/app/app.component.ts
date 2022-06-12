eb0217eaf07e04969b2a1fbf12cec5b144b73c1c
nkmung <mungdola999@gmail.com>
2022-06-12 16:39:40 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'demo-app-train';
+  name: string = '';
+  inputFormControl = new FormControl('');
+  addTodo() {
+    console.log(this.name);
+  }
+  onclick(){
+    console.log(this.inputFormControl);
+  }
 }
